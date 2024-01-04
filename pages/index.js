81b6e71ffc352e323127f8bb0fe830b11646b16e
lincesgg <87877330+lincesgg@users.noreply.github.com>
2024-01-04 20:05:08 +0000

    Adicionando Pasta de Páginas do Site

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function home()
+{
+    return <h1> <b> Vicente </b>, se você não estiver codando quando abrir isso</h1>
+    return <h1> Eu vou aparecer debaixo da sua cama, de noite</h1>
+    return <h1> E puxar seu pé</h1>
+}
+
+export default home;
