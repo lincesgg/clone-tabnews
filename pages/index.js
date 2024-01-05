cbce23b5a7eb5f06a57dc624df83c6670f94fed5
lincesgg <87877330+lincesgg@users.noreply.github.com>
2024-01-05 23:26:22 +0000

    Modificando mensagem de index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
+import localFont from '@next/font/local'
+
+const heavitas = localFont({src: "../public/heavitas/heavitas-webfont.woff2"})
+
 function home()
 {
-    return <h1> <b> Vicente </b>, se você não estiver codando quando abrir isso</h1>
-    return <h1> Eu vou aparecer debaixo da sua cama, de noite</h1>
-    return <h1> E puxar seu pé</h1>
+    const styles = {
+        txt: {
+            "fontFamily": "heavitas"
+        }
+    }
+    
+    return (
+        // <div style={{display:"flex", alignItems:"center", justifyContent:"center", width:"100%", height:"100%"}}>
+        <div>
+            <h1 style={styles.txt}> Mamãe, você é um amor 😍💖💗</h1>
+            {/* <h1 style={styles.txt}> <b> Vicente 0-0 </b>, se você não estiver codando 👨‍💻 quando abrir isso</h1> */}
+            {/* <h1 style={styles.txt}> Eu vou aparecer debaixo da sua cama 🛏️, de noite 🌙</h1> */}
+            {/* <h1 style={styles.txt}> E puxar seu pé 🦶🏻🔪</h1> */}
+        </div>
+    );
 }
 
 export default home;
